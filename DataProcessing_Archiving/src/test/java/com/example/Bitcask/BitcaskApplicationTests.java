7304330006d52d07739528e7d4cf3ccb105361c1
ayaelsayed25 <ayaelsayed52092@gmail.com>
2023-05-13 19:46:19 +0300

    Bitcask

---
+++ DataProcessing_Archiving/src/test/java/com/example/Bitcask/BitcaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bitcask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
